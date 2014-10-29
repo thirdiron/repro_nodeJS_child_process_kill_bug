500d6e7fa1b8ced4f96280c23df5ae47ca47671f
Michael Lang <michaellang@Michaels-MacBook-Pro.local>
2014-10-29 15:19:58 -0500

    Testing with spawn instead of exec.
    Apparently it doesn't repro using spawn!

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var child_process = require('child_process');
 
+process.on('uncaughtException', function(err) {
+  debugger;
+  console.log(err.stack);
+  process.exit();
+});
+
 var myUnkillableChild;
 
 myUnkillableChild =
-      child_process.exec('node ./child_root/child.js',
-                     {},
-                     function(err, stdout, stderr) {
-                       debugger;
-                       if (err) console.log(err);
-                       console.log('My child process should be dead');
-                     });
+      child_process.spawn('node', ['./child_root/child.js']);
 
 console.log('Hello world! A child should be running now');
 debugger;
@@ -17,6 +17,7 @@ debugger;
 setTimeout(function() {
   // Wait for a few seconds to ensure the server is up and running
   // Then kill it and exit
+  debugger;
   console.log('Sending SIGTERM signal to child process!');
   myUnkillableChild.kill('SIGTERM');
   debugger;
